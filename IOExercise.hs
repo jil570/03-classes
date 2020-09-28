42a0c059f26eff53fe23b423182476845ce0a84b
jil570 <jil570@ucsd.edu>
2020-09-28 13:29:05 -0400

    Update

--- IOExercise.hs
+++ IOExercise.hs
@@ -1,25 +1,54 @@
 import System.FilePath
 
 simpleProgram :: IO ()
-simpleProgram = do
+-- simpleProgram = do
+--   putStrLn "This is a simple program that does IO."
+--   putStrLn "What is your name?"
+--   inpStr <- getLine
+--   putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
+simpleProgram =
   putStrLn "This is a simple program that does IO."
-  putStrLn "What is your name?"
-  inpStr <- getLine
-  putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
+    >> putStrLn "What is your name?"
+    >> getLine
+      >>= ( \inpStr ->
+              putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
+          )
 
 lengthProgram :: IO Int
-lengthProgram = do
+-- lengthProgram = do
+--   let x = length [1, 2, 3, 4, 5, 6]
+--   putStrLn $ "The length of the list is" ++ show x
+--   return x
+-- lengthProgram = do
+--   let x = length [1, 2, 3, 4, 5, 6]
+--   putStrLn $ "The length of the list is" ++ show x
+--   return x
+
+lengthProgram =
   let x = length [1, 2, 3, 4, 5, 6]
-  putStrLn $ "The length of the list is" ++ show x
-  return x
+   in putStrLn
+        ( "The length of the list is " ++ show x
+        )
+        >> return x
 
 anotherProgram :: IO ()
-anotherProgram = do
+-- anotherProgram = do
+--   putStrLn "What is your name?"
+--   inpStr <- getLine
+--   if inpStr == "Haskell"
+--     then do
+--       putStrLn "You rock!"
+--       return ()
+--     else putStrLn $ "Hello " ++ inpStr
+--   putStrLn "That's all!"
+
+anotherProgram =
   putStrLn "What is your name?"
-  inpStr <- getLine
-  if inpStr == "Haskell"
-    then do
-      putStrLn "You rock!"
-      return ()
-    else putStrLn $ "Hello " ++ inpStr
-  putStrLn "That's all!"
+    >> getLine
+      >>= ( \inpStr ->
+              ( if inpStr == "Haskell"
+                  then putStrLn "You rock!"
+                  else putStrLn $ "Hello " ++ inpStr
+              )
+                >> putStrLn "That's all!"
+          )
